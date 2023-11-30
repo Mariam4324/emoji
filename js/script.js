8d5436d82a93dfa2d4c057a49f60805e75a27303
Mariam4324 <ashkmariam@gmail.com>
2023-11-30 17:35:58 +0300

    2 commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
-//меняем класс 
-listNode.className = "lala";
-//к классу добавляется новый класс рядом, главное не забыть пробел 
-listNode.className += " lala";
-//
-listNode.classList;
-
-// add, remove, contains, toggle, replace
-
-// добавляет класс ul
-listNode.classList.add("ul");
-
-//удаляет
-listNode.classList.remove("list");
-
-// есть ли какой класс или нет, возвращает булевый
-listNode.classList.contains("list");
-
-// заменяет один класс на другой
-listNode.classList.replace("list", "super__list");
-
-//принимает 1 параметр, если класс опенд есть он его удалить, если его нет то он его добавит
-listNode.classList.toggle("opened");
-
+// <!-- Получить коллекцию элементов списка,
+//       проверить, что число после _ является четным, если это так
+//       покрасить фон элемента.
+//       -->

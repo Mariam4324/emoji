3c726d8c5e841eca3bcf056f49fcb4f3e5670cfe
Mariam4324 <ashkmariam@gmail.com>
2023-12-11 15:59:53 +0300

    23 commit

--- js/script.js
+++ js/script.js
@@ -2,22 +2,38 @@ import { emojies } from "./emojies.js";
 let input = document.querySelector(".input");
 let wrapper = document.querySelector(".finder__wrapper");
 
-// перебираем массив и параметром принимаем все объекты этого массива
-// очищаем обертку карточек
-// суем в начало обертки функцию с разметками карточек, параметром берем каждый объект
-// а в саму функцию renderEmoji передаем параметром arr, а при вызове функции аргументов принимаем массив emojies
+// function deleteDublicates(arr) {
+//   arr.map((el) => {
+//     const keywordsArr = el.keywords.split(" ");
+//     const unicValuesArr = [...new Set(keywordsArr)];
+//     const unicValuesString = unicValuesArr.join(" ");
+//   });
+// }
+// deleteDublicates(emojies);
+
+function deleteDublicates(string) {
+  const keywordsArr = string.split(" ");
+  const unicValuesArr = [...new Set(keywordsArr)];
+  const unicValuesString = unicValuesArr.join(" ");
+  return unicValuesString;
+}
+
+// // перебираем массив и параметром принимаем все объекты этого массива
+// // очищаем обертку карточек
+// // суем в начало обертки функцию с разметками карточек, параметром берем каждый объект
+// // а в саму функцию renderEmoji передаем параметром arr, а при вызове функции аргументов принимаем массив emojies
 function renderEmoji(arr) {
   wrapper.innerHTML = ` `;
+
   arr.forEach((obj) => {
     wrapper.append(createCard(obj));
   });
 }
-
-// вытаскиваем карточку и все элементы карточки
-// задаем им классы
-// присваиваем каждому элементу ключ объекта
-// суем все элементы карточки в саму карточку
-// на выходе возвращаем карточку
+// // вытаскиваем карточку и все элементы карточки
+// // задаем им классы
+// // присваиваем каждому элементу ключ объекта
+// // суем все элементы карточки в саму карточку
+// // на выходе возвращаем карточку
 
 function createCard(obj) {
   const card = document.createElement("div");
@@ -32,19 +48,19 @@ function createCard(obj) {
 
   emojie.textContent = obj.symbol;
   name.textContent = obj.title;
-  info.textContent = obj.keywords;
-
+  info.textContent = deleteDublicates(obj.keywords);
   card.append(emojie, name, info);
+
   return card;
 }
 
-// ставим слушатель событий на функцию search и параметром передаем событие. Создаем inputValue равный целевому обьекту (самому инпуту), создаем переменную с фильтром по нужному условию  и передаем параметром эту переменную в функцию renderEmoji
+// // ставим слушатель событий на функцию search и параметром передаем событие. Создаем inputValue равный целевому обьекту (самому инпуту), создаем переменную с фильтром по нужному условию  и передаем параметром эту переменную в функцию renderEmoji
 function search(evt) {
   const inputValue = evt.target.value;
   const filter = emojies.filter(
     (el) =>
-      el.title.toLowerCase().trim().includes(inputValue.toLowerCase().trim()) ||
-      el.keywords.toLowerCase().trim().includes(inputValue.toLowerCase().trim())
+      el.title.toLowerCase().includes(inputValue.toLowerCase().trim()) ||
+      el.keywords.toLowerCase().includes(inputValue.toLowerCase().trim())
   );
 
   renderEmoji(filter);
